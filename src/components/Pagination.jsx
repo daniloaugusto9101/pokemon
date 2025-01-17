99cba2a62a3d41ffab47d57ada62f3b86ad12b9e
Danilo Augusto <danilo.augusto@artefatodigital.com>
2025-01-17 20:10:02 -0300

    fix: Correção de classes e botão SVG

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -3,20 +3,20 @@ import React from "react";
 const Pagination = () => {
   return (
     <footer className="my-14 flex justify-center">
-      <div class="flex items-center space-x-2 text-gray-600">
-        <button class="px-3 py-1 text-sm rounded hover:bg-gray-200 disabled:text-gray-400 disabled:cursor-not-allowed" disabled>
+      <div className="flex items-center space-x-2 text-gray-600">
+        <button className="px-3 py-1 text-sm rounded hover:bg-gray-200 disabled:text-gray-400 disabled:cursor-not-allowed" disabled>
           &larr; Anterior
         </button>
 
-        <button class="px-3 py-1 text-sm rounded hover:bg-gray-200">1</button>
-        <button class="px-3 py-1 text-sm font-semibold bg-gray-300 text-gray-900 rounded">2</button>
-        <button class="px-3 py-1 text-sm rounded hover:bg-gray-200">3</button>
-        <button class="px-3 py-1 text-sm rounded hover:bg-gray-200">4</button>
-        <button class="px-3 py-1 text-sm rounded hover:bg-gray-200">5</button>
-        <span class="px-2 py-1 text-sm text-gray-400">...</span>
-        <button class="px-3 py-1 text-sm rounded hover:bg-gray-200">11</button>
+        <button className="px-3 py-1 text-sm rounded hover:bg-gray-200">1</button>
+        <button className="px-3 py-1 text-sm font-semibold bg-gray-300 text-gray-900 rounded">2</button>
+        <button className="px-3 py-1 text-sm rounded hover:bg-gray-200">3</button>
+        <button className="px-3 py-1 text-sm rounded hover:bg-gray-200">4</button>
+        <button className="px-3 py-1 text-sm rounded hover:bg-gray-200">5</button>
+        <span className="px-2 py-1 text-sm text-gray-400">...</span>
+        <button className="px-3 py-1 text-sm rounded hover:bg-gray-200">11</button>
 
-        <button class="px-3 py-1 text-sm rounded hover:bg-gray-200 disabled:text-gray-400 disabled:cursor-not-allowed">Próximo &rarr;</button>
+        <button className="px-3 py-1 text-sm rounded hover:bg-gray-200 disabled:text-gray-400 disabled:cursor-not-allowed">Próximo &rarr;</button>
       </div>
     </footer>
   );
--- src/components/PokemonDetail.jsx
+++ src/components/PokemonDetail.jsx
@@ -3,20 +3,16 @@ import { FaFrog } from "react-icons/fa";
 
 const PokemonDetail = () => {
   return (
-    <div class="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
-      <div class="bg-white w-[500px] rounded-2xl shadow-lg relative">
+    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
+      <div className="bg-white w-[500px] rounded-2xl shadow-lg relative">
         {/* Barra de titulo */}
-        <div class="bg-gray-200 rounded-t-2xl p-4 relative">
-          <h2 class="text-lg text-gray-800 font-bold text-center">Pokemon name</h2>
-          <button class="absolute top-4 right-4 text-gray-500 hover:text-gray-700">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
-              <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-            </svg>
-          </button>
+        <div className="bg-gray-200 rounded-t-2xl p-4 relative">
+          <h2 className="text-lg text-gray-800 font-bold text-center">Pokemon name</h2>
+          <button className="absolute top-4 right-4 text-gray-500 hover:text-gray-700">X</button>
         </div>
 
         {/*Corpo do modal  */}
-        <div class="bg-yellow-300  rounded-b-2xl flex flex-col items-center gap-8 p-8">
+        <div className="bg-yellow-300  rounded-b-2xl flex flex-col items-center gap-8 p-8">
           {/* imaggem */}
           <div className="w-56 h-56 flex items-center justify-center bg-gray-200 rounded-t-lg bg-green-300">
             <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png" alt="Bulbasaur" className="w-24 h-24" />
@@ -54,8 +50,8 @@ const PokemonDetail = () => {
           </div>
 
           {/* Descrição */}
-          <p class="text-gray-600 mb-6 bg-blue-300">Este é o conteúdo do modal. Você pode adicionar textos, botões ou qualquer outro elemento aqui.</p>
-          <p class="text-gray-600 mb-6 bg-blue-300">lorem lorem</p>
+          <p className="text-gray-600 mb-6 bg-blue-300">Este é o conteúdo do modal. Você pode adicionar textos, botões ou qualquer outro elemento aqui.</p>
+          <p className="text-gray-600 mb-6 bg-blue-300">lorem lorem</p>
         </div>
       </div>
     </div>
