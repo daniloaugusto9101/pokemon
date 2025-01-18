5b07045d3cdec18e8f516886c6f28e7c6cba4813
Danilo Augusto <danilo.augusto@artefatodigital.com>
2025-01-18 17:53:00 -0300

    refator: Ajuste de layout

--- src/components/PokemonItem.jsx
+++ src/components/PokemonItem.jsx
@@ -1,23 +1,26 @@
 import React from "react";
 import { FaFrog } from "react-icons/fa";
 
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
 const PokemonItem = ({ pokemon }) => {
   return (
     <div className="w-64 h-72 border border-gray-300 rounded-lg shadow-md flex flex-col items-center justify-between ">
       {/* Imagem do Pokémon */}
       <div className="w-full h-2/3 flex items-center justify-center bg-gray-200 rounded-t-lg">
-        <img src={pokemon.sprites.front_default} alt="Bulbasaur" className="w-24 h-24" />
+        <img src={pokemon.sprites.versions["generation-v"]["black-white"].animated.front_default} alt="Pokemon GIF" className="w-20" />
       </div>
 
       {/* Conteúdo do Card */}
       <div className="w-full flex justify-between space-y-1 p-6">
         <div className="flex flex-col justify-between">
-          <h2 className="text-lg font-bold text-gray-700">{pokemon.name}</h2>
+          <h2 className="text-lg font-bold text-gray-700">{capitalizeFirstLetter(pokemon.name)}</h2>
           <p className="text-sm text-gray-600">Tipo {pokemon.types[0].type.name}</p>
         </div>
-        <div className="flex flex-col items-center justify-between">
-          <FaFrog className="text-gray-500" />
-          <p className="text-sm font-medium text-gray-500">{pokemon.species.name}</p>
+        <div className="flex flex-col items-center justify-center">
+          <img src={pokemon.sprites.versions["generation-i"]["red-blue"].front_default} alt="Pokemon GIF" className="w-8" />
         </div>
       </div>
     </div>
--- src/components/PokemonList.jsx
+++ src/components/PokemonList.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import PokemonItem from "./PokemonItem";
+import { TbPokeball } from "react-icons/tb";
 import { GlobalContext } from "../contexts/GlobalContext";
 
 const PokemonList = () => {
   const { pokemonDetails, total } = React.useContext(GlobalContext);
   if (pokemonDetails) {
     return (
-      <main className="flex justify-center mt-28 ">
+      <main className="flex justify-center mt-14 ">
         <section className="w-[1140px]">
           <header>
-            <p>Total: {total} Pokémons</p>
+            <p className="flex items-center text-lg text-gray-600 gap-2">
+              <TbPokeball className="text-xl " /> Total: {total} Pokémons
+            </p>
           </header>
           <div className="mt-14 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 justify-items-center">
             {pokemonDetails.map((pokemon) => {
