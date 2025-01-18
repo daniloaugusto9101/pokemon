b92b5748934af0fc7570ea2e54bdf243e4a2e467
Danilo Augusto <danilo.augusto@artefatodigital.com>
2025-01-18 09:18:47 -0300

    feat: Add Request Pokemon details

--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -1,47 +1,32 @@
 import React from "react";
 import SearchPokemon from "./SearchPokemon";
 import PokemonList from "./PokemonList";
-import useFetch from "../hooks/useFetch";
 import useGetPokemons from "../hooks/useGetPokemons";
 
 const Pokemon = () => {
-  const { loading, error, request } = useFetch();
   const [pokemonDetails, setPokemonDetails] = React.useState([]);
   const [searchQuery, setSearchQuery] = React.useState("");
 
   const searchResults = [];
+
+  //Recupera os pokemons
   const pokemons = useGetPokemons();
-  console.log("Aqui: ", pokemons);
+  console.log("Aqui: ", pokemonDetails);
 
-  //Faz a busca da lista de Pokemon
   React.useEffect(() => {
-    const fetchPokemonDetails = async () => {
-      const { json } = await request("https://pokeapi.co/api/v2/pokemon?limit=20");
+    setPokemonDetails(pokemons);
+  }, [pokemons]);
 
-      if (json && json.results) {
-        // Realizar as requisições individuais para cada Pokémon
-        const pokemonDetails = await Promise.all(
-          json.results.map(async (pokemon) => {
-            const { json: details } = await request(pokemon.url);
-            return details;
-          })
-        );
-        setPokemonDetails(pokemonDetails);
-      }
-    };
-
-    fetchPokemonDetails();
-  }, [request]);
-
+  // Recupera texto digitado no campo de pesquisa
   function handleOnSearch(pokemonName) {
-    console.log(pokemonName);
+    // console.log(pokemonName);
     setSearchQuery(pokemonName);
   }
 
   return (
     <>
-      <SearchPokemon onSearch={handleOnSearch} />
-      <PokemonList pokemonDetails={searchResults.length > 0 ? searchResults : pokemonDetails} />
+      <SearchPokemon onSearch={handleOnSearch} /> {/* Campo de pesquisa */}
+      <PokemonList pokemonDetails={searchResults.length > 0 ? searchResults : pokemonDetails} /> {/* Lista de pokemons */}
     </>
   );
 };
--- src/hooks/useGetPokemons.jsx
+++ src/hooks/useGetPokemons.jsx
@@ -1,12 +1,28 @@
 import React from "react";
 import PokemonsService from "../api/PokemonsService";
+import useFetch from "./useFetch";
 
 const useGetPokemons = () => {
   const [pokemons, setPokemons] = React.useState([]);
+  const { request } = useFetch();
 
   React.useEffect(() => {
-    PokemonsService.getPokemons().then(({ data }) => setPokemons(data.results));
-  }, []);
+    const fetchPokemons = async () => {
+      const { data } = await PokemonsService.getPokemons();
+
+      if (data && data.results) {
+        const detailedPokemons = await Promise.all(
+          data.results.map(async (pokemon) => {
+            const { json: details } = await request(pokemon.url);
+            return details;
+          })
+        );
+        setPokemons(detailedPokemons);
+      }
+    };
+
+    fetchPokemons();
+  }, [request]);
 
   return pokemons;
 };
