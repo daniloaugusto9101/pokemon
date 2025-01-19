1a740133b3e633696d39819bdf57129b23ca8fc8
Danilo Augusto <danilo.augusto@artefatodigital.com>
2025-01-19 17:19:58 -0300

    feat: Implementado efeito hover no card e corrigido bug de clique

--- src/components/PokemonItem.jsx
+++ src/components/PokemonItem.jsx
@@ -14,24 +14,26 @@ const PokemonItem = ({ pokemon }) => {
   };
 
   return (
-    <div className="w-64 h-72 border border-gray-300 rounded-lg shadow-md flex flex-col items-center justify-between " onClick={handleCardClick}>
-      {/* Imagem do Pokémon */}
-      <div className="w-full h-2/3 flex items-center justify-center bg-gray-200 rounded-t-lg">
-        <img src={pokemon.sprites.versions["generation-v"]["black-white"].animated.front_default} alt="Pokemon GIF" className="w-20" />
-      </div>
+    <>
+      <div className="w-64 h-72 border border-gray-300 rounded-lg shadow-md flex flex-col items-center justify-between cursor-pointer transform transition-transform duration-300 hover:scale-105 hover:shadow-lg hover:shadow-gray-500/50" onClick={handleCardClick}>
+        {/* Imagem do Pokémon */}
+        <div className="w-full h-2/3 flex items-center justify-center bg-gray-200 rounded-t-lg">
+          <img src={pokemon.sprites.versions["generation-v"]["black-white"].animated.front_default} alt="Pokemon GIF" className="w-20" />
+        </div>
 
-      {/* Conteúdo do Card */}
-      <div className="w-full flex justify-between space-y-1 p-6">
-        <div className="flex flex-col justify-between">
-          <h2 className="text-lg font-bold text-gray-700">{capitalizeFirstLetter(pokemon.name)}</h2>
-          <p className="text-sm text-gray-600">Tipo {pokemon.types[0].type.name}</p>
-        </div>
-        <div className="flex flex-col items-center justify-center">
-          <img src={pokemon.sprites.versions["generation-i"]["red-blue"].front_default} alt="Pokemon GIF" className="w-8" />
+        {/* Conteúdo do Card */}
+        <div className="w-full flex justify-between space-y-1 p-6">
+          <div className="flex flex-col justify-between">
+            <h2 className="text-lg font-bold text-gray-700">{capitalizeFirstLetter(pokemon.name)}</h2>
+            <p className="text-sm text-gray-600">Tipo {pokemon.types[0].type.name}</p>
+          </div>
+          <div className="flex flex-col items-center justify-center">
+            <img src={pokemon.sprites.versions["generation-i"]["red-blue"].front_default} alt="Pokemon GIF" className="w-8" />
+          </div>
         </div>
       </div>
-      {isModalOpen && <PokemonDetail pokemon={pokemon} handleCloseModal={handleCloseModal} />}
-    </div>
+      {isModalOpen && <PokemonDetail pokemon={pokemon} handleCardClick={handleCloseModal} />}
+    </>
   );
 };
 
